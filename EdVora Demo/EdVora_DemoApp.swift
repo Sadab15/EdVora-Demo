b2858d51673e5a854191331d169a870c38e3ad52
fazil1979 <fazil@differenz.co.in>
2021-11-25 15:52:11 +0530

    Initial Commit

---
+++ EdVora Demo/EdVora_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdVora_DemoApp.swift
+//  EdVora Demo
+//
+//  Created by differenz242 on 25/11/21.
+//
+
+import SwiftUI
+
+@main
+struct EdVora_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
